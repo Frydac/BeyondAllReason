9a73c31da066385020c32a2ad3ef30826d12d6ff
Emile Vrijdags <emile.vrijdags@newauro.com>
2024-04-07 17:08:46 +0200

    add no_heavy_t3

---
+++ no_heavy_t3.lua
@@ -0,0 +1,32 @@
+local units_to_remove = {
+    "armbanth", -- "Titan",
+    "armthor", -- "Thor",
+    "corjugg", -- "Behemoth",
+    "corkorg", -- "Juggernaut",
+    "corcrw", -- "Dragon",
+    "corcrwh", -- "Dragon", not sure what the difference is
+}
+
+-- makes lookup more efficient/convenient
+local function array_to_lookup_table(array)
+    local lookup_table = {}
+    for _, value in ipairs(array) do
+        lookup_table[value] = true
+    end
+    return lookup_table
+end
+
+units_to_remove = array_to_lookup_table(units_to_remove)
+
+for name, ud in pairs(UnitDefs) do
+    if type(ud.buildoptions) == "table" then
+        for i = #ud.buildoptions, 1, -1 do
+            if units_to_remove[ud.buildoptions[i]] then
+                table.remove(ud.buildoptions, i)
+            end
+        end
+    end
+end
+
+
+
